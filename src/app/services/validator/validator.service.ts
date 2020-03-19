0f2120b7e28944fc5d6907bb13ff7fecb9aff218
RutujaSatkar96 <60915608+RutujaSatkar96@users.noreply.github.com>
2020-03-19 12:17:16 -0700

    added change for service

--- src/app/services/validator/validator.service.ts
+++ src/app/services/validator/validator.service.ts
@@ -12,7 +12,8 @@ export class ValidatorService {
   constructor(private http: HTTP, private platform: Platform, private toastController: ToastController) { }
 
   validateForm(smartScanForm: SmartScanForm) {
-    const url = 'http://13.75.106.59/syntbots-ai/humana_service2/ai/extract';
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+    const url = 'http://13.75.106.59:5003/ai/humanaextract1';
     let formData:FormData = new FormData();
 
     formData.append('file', this.B64toBlob(smartScanForm.FormPages[0].imageData), 'file.name' + Math.random() + '.jpg');
@@ -29,7 +30,7 @@ export class ValidatorService {
       });
     } else {
       return new Promise((resolve, reject) => {
-        fetch(url, {
+        fetch(proxyUrl+url, {
             body: formData,
             method: 'POST'
         })
